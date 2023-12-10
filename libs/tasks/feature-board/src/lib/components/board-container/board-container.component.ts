cf34ba0c9670d25c8ce82f1944a386a1dcf3f0ca
Railan Barbosa <railantb@gmail.com>
2023-12-10 18:53:23 +0000

    Add LoadingSpinner in Board Container

--- libs/tasks/feature-board/src/lib/components/board-container/board-container.component.ts
+++ libs/tasks/feature-board/src/lib/components/board-container/board-container.component.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { tap } from 'rxjs';
 import { CardComponent, HeaderComponent, LoadingSpinnerComponent } from 'shared/ui/components';
+import { LoadingService } from 'shared/util/services';
 import { TaskService } from '../../services/task.service';
 
 @Component({
@@ -14,8 +15,12 @@ import { TaskService } from '../../services/task.service';
     templateUrl: './board-container.component.html',
     styleUrl: './board-container.component.scss'
 })
-export class BoardContainerComponent {
+export class BoardContainerComponent implements OnInit {
     private taskService = inject(TaskService);
+    private loadingService = inject(LoadingService);
+
+    loading$ = this.loadingService.isLoading$;
+    showContent = false;
 
     todoTasks: any[] = [{ title: 'Task 1', description: 'Lorem Ipsum' }];
     doingTasks: any[] = [{ title: 'Task 1', description: 'Lorem Ipsum' }, { title: 'Task 2', description: 'Lorem Ipsum' }];
@@ -29,5 +34,13 @@ export class BoardContainerComponent {
                 }),
             )
             .subscribe();
+    }
+
+    public ngOnInit(): void {
+        this.loadingService.show();
+        setTimeout(() => {
+            this.loadingService.hide();
+            this.showContent = true;
+        }, 2000);
     }
 }
